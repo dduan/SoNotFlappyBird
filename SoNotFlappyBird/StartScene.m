f60267470bdbf82b8e17867f57df7a43798b5f60
Daniel Duan <danmarner@gmail.com>
2014-03-16 23:36:14 -0500

    removed magic numbers.

--- SoNotFlappyBird/StartScene.m
+++ SoNotFlappyBird/StartScene.m
@@ -13,6 +13,12 @@
 @property BOOL isPlaying;
 @end
 
+
+#define kGravityConstantY -11.0f
+#define kBirdSize 44.0f
+#define kBirdVelocityY 530.0f
+
+
 @implementation StartScene
 
 - (void)didMoveToView:(SKView *)view
@@ -26,7 +32,7 @@
 - (void)createSceneContents
 {
     self.scaleMode = SKSceneScaleModeAspectFit;
-    self.physicsWorld.gravity = CGVectorMake(0, -11.0);
+    self.physicsWorld.gravity = CGVectorMake(0, kGravityConstantY);
     
     SKPhysicsBody *border = [SKPhysicsBody bodyWithEdgeLoopFromRect: self.frame];
     self.physicsBody = border;
@@ -40,8 +46,8 @@
 - (SKSpriteNode *)newBird
 {	
     SKSpriteNode *bird = [SKSpriteNode spriteNodeWithImageNamed: @"bird"];
-    bird.size = CGSizeMake(44.0, 44.0);
-    bird.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius: 22.0];
+    bird.size = CGSizeMake(kBirdSize, kBirdSize);
+    bird.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius: kBirdSize / 2];
     bird.physicsBody.dynamic = NO;
     bird.name = @"bird";
     return bird;
@@ -54,7 +60,7 @@
         self.isPlaying = YES;
         bird.physicsBody.dynamic = YES;
     }
-    bird.physicsBody.velocity = CGVectorMake(0, 530);
+    bird.physicsBody.velocity = CGVectorMake(0, kBirdVelocityY);
 
 }
 @end
