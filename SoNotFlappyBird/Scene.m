2bba3e4f139ea74e337ddb29c524c6b51e583d7f
Daniel Duan <danmarner@gmail.com>
2014-03-21 00:14:29 -0500

    fixed a animation bug

--- SoNotFlappyBird/Scene.m
+++ SoNotFlappyBird/Scene.m
@@ -144,6 +144,7 @@
         [node removeFromParent];
     }];
     self.bird.position = CGPointMake(kBirdPositionX, CGRectGetMidY(self.frame));
+    [self.bird removeAllActions];
     [self.bird runAction: [SKAction rotateToAngle: 0.0f duration: 0]];
     self.bird.physicsBody.dynamic = NO;
     self.isEnded = NO;
